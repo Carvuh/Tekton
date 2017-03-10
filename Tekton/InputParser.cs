7e29978b07b5a4fedc96ee117699479e00106b4d
Justin Carver <Justin Carver>
2017-03-09 19:20:58 -0600

    Implemented error exceptions. A shitty way, but something for now...

--- Tekton/InputParser.cs
+++ Tekton/InputParser.cs
@@ -8,11 +8,44 @@ namespace Tekton
 {
     class InputParser
     {
+
+        public enum parserErrors {ARG_ERROR, TYPE_ERROR};
+
+        static char[] delimiterChars = {' ', ',', '.', ':', ';', '?', '!', '\t'};
+        static string[] messageArray;
+
         public static void ParseInput(string message) {
-            Console.WriteLine("Message recieved was!: " + message);
+            message = message.ToLower();
+            messageArray = message.Split(delimiterChars);
+            Console.WriteLine("Message recieved was!: " + messageArray[0]);
             // Do whatever we need to do to parse something.
-            if (message == "q" || message == "quit" || message == "exit") {
+            if (messageArray[0] == "addentry") {
+                if (messageArray[1] != null)
+                {
+                    Console.WriteLine("Adding a new Entry to Page of: " + messageArray[1] + "!");
+                }
+                else {
+                    ErrorException(parserErrors.ARG_ERROR);
+                }
+            }
+            // Application Specific Arguemnts ---------------------------
+            else if (messageArray[0] == "q" || messageArray[0] == "quit" || messageArray[0] == "exit") {
                 Environment.Exit(0);
+            }
+            else if (messageArray[0] == "clear" || messageArray[0] == "cls") {
+                Console.Clear();
+            }
+            else {
+                ErrorException(parserErrors.ARG_ERROR);
+            }
+        }
+
+        public static void ErrorException(parserErrors error) {
+            if (error == parserErrors.ARG_ERROR) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a valid set of arguments!");
+                Console.ResetColor();
+                return;
             }
         }
 
