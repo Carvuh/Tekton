b82d2b8eb9cc09bb87558815328c10bf34b77841
Justin Carver <Justin Carver>
2017-03-09 18:36:15 -0600

    Removed excess program files.

--- Tekton/Tekton.cs
+++ Tekton/Tekton.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tekton {
     class Tekton {
+
+        // GLOBALS
+        //
+
+        static string currentProjectName;
+        static string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        static bool isProjectMode = false;
 
         static void Main(string[] args) {
             if (args.Length <= 0) {
@@ -18,12 +26,51 @@ namespace Tekton {
                     Console.WriteLine(args[1].ToString());
                 }
                 if (args[0] == "-np") {
-                    // -np = -NewProject
-                    // We're going to initilize the project creation.
+                    // New Project:
+                    // [args]
+                    // e.g. "-np ProjectName"
+                    // -----
+                    // We're going to initilize the project creation. This will create the .world file for the project.
                     string projectName = args[1].ToString();
-
+                    isProjectMode = !isProjectMode;
+                    currentProjectName = projectName;
+                    Directory.CreateDirectory(projectName);
+                    Directory.SetCurrentDirectory(projectName);
+                    File.WriteAllText(projectName + ".world", "");
+                    Console.WriteLine("Creating folder " + projectName + " at directory: " + Directory.GetCurrentDirectory());
+                    // Afterwards, we are going to init the project's first envrionment.
+                    // Making sure the user is in their correct folder, editing the correct things.
+                    TektonProjectCLI(projectName, userName);
                 }
             }
+
+            // If the user is inside their project working on it.
+            // Keep restarting the parser on enter;
+            if (isProjectMode){
+                TektonProjectCLI(currentProjectName, userName);
+            }
+
+        }
+
+        public static void ImplementCustomHeader(string projectName, string userName) {
+            // Create a custom header for the world to see things neatly...
+            // Hoping this works. Damn Windows CMD... Always screwing colors up...
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(userName);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(":" + projectName);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(" ~");
+            Console.ResetColor();
+            Console.Write(" > ");
+        }
+
+        public static void TektonProjectCLI(string projectName, string userName) {
+            ImplementCustomHeader(projectName, userName);
+            string something = Console.ReadLine();
+            InputParser.InputManager(something);
         }
 
         public static void GetConsoleInput() {
@@ -37,9 +84,22 @@ namespace Tekton {
             Console.Read();
         }
 
+        public static string GetCurrentProject() {
+            return currentProjectName;
+        }
+
+        public static string GetUserName() {
+            return userName;
+        }
+
         public static void StartTekton() {
             Console.Clear();
-            GetConsoleInput();   
+            GetConsoleInput();  
+        }
+
+        // When the process closes.
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e) {
+            Console.ResetColor();
         }
 
     }
