562e5435d81f1987789d9e1ad7b1a2c5cf5d4d65
Justin Carver <Justin Carver>
2017-03-09 18:29:32 -0600

    Adding base files.

---
+++ Tekton/InputParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekton
+{
+    class InputParser
+    {
+        public static void ParseInput(string message) {
+            Console.WriteLine("Message recieved was!: " + message);
+            // Do whatever we need to do to parse something.
+            if (message == "q" || message == "quit" || message == "exit") {
+                Environment.Exit(0);
+            }
+        }
+
+        public static void InputManager(string userInput) {
+            ParseInput(userInput);
+            Tekton.TektonProjectCLI(Tekton.GetCurrentProject(), Tekton.GetUserName());
+        }
+    }
+}
---
+++ Tekton/Tekton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekton {
+    class Tekton {
+
+        static void Main(string[] args) {
+            if (args.Length <= 0) {
+                StartTekton();
+            }
+            else {
+                if (args[0] == "-ne") {
+                    // -ne = -NewEntry
+                    // We're going to append a new entry to the specified wiki page.
+                    Console.WriteLine(args[1].ToString());
+                }
+                if (args[0] == "-np") {
+                    // -np = -NewProject
+                    // We're going to initilize the project creation.
+                    string projectName = args[1].ToString();
+
+                }
+            }
+        }
+
+        public static void GetConsoleInput() {
+            Console.Write("> ");
+            string parsedInformation = Console.ReadLine();
+        }
+
+        public static void GetConsoleInput(string premessage) {
+            string prompt = "> ";
+            Console.WriteLine(premessage +  " " + prompt);
+            Console.Read();
+        }
+
+        public static void StartTekton() {
+            Console.Clear();
+            GetConsoleInput();   
+        }
+
+    }
+}
